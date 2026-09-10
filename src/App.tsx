2dc85fbbc0ab2da2dd6eb51b0cbb906defc1babd
abhirathsmadhav <abhirathsmadhav@gmail.com>
2026-09-11 01:05:03 +0530

    feat: move ad popup to global scope excluding admin dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import HomePage from './pages/HomePage';
 import VotePage from './pages/VotePage';
 import LoginPage from './pages/LoginPage';
 import AdminDashboardPage from './pages/AdminDashboardPage';
 import AdminRoute from './components/AdminRoute';
 import Navbar from './components/Navbar';
+import AdPopup from './components/AdPopup';
 
 function App() {
+  const [showGlobalAd, setShowGlobalAd] = useState(false);
+
+  useEffect(() => {
+    const adInterval = setInterval(() => {
+      // Don't show on admin routes
+      if (!window.location.pathname.startsWith('/admin')) {
+        setShowGlobalAd(true);
+      }
+    }, 10 * 60 * 1000); // 10 minutes
+    
+    return () => clearInterval(adInterval);
+  }, []);
+
   return (
     <>
       <Navbar />
@@ -25,6 +40,7 @@ function App() {
           }
         />
       </Routes>
+      <AdPopup show={showGlobalAd} onClose={() => setShowGlobalAd(false)} />
     </>
   );
 }
